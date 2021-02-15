57f16852608e9e21f56dc5795f21facb1255cbc7
HP <hd156681@gmail.com>
2021-02-16 01:00:49 +0530

    My first Commit

---
+++ src/main/java/Myclass.java
@@ -0,0 +1,11 @@
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello JEnkins and Git hub !!");
+		System.out.println("HEllo Quicks !!");
+
+	}
+
+}
