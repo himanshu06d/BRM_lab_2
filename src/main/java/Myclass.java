842221f3b145e534ef940bcd4ed64699e638b9f0
HP <hd156681@gmail.com>
2021-02-16 11:07:10 +0530

    third commit

--- src/main/java/Myclass.java
+++ src/main/java/Myclass.java
@@ -4,8 +4,9 @@ public class Myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello JEnkins and Git hub !!");
-		System.out.println("HEllo Quicks !!");
+		
 		System.out.println("via automation");
+		System.out.println("my third");
 
 	}
 
